d65bb1f223196d435c747c765161ea972a8ed942
Håkon <hrr1@hotmail.no>
2021-10-18 19:34:45 +0200

    added warning to developers and empty a3a class to missionConfigFile

--- A3A/addons/core/CfgFunctions.hpp
+++ A3A/addons/core/CfgFunctions.hpp
@@ -1,3 +1,4 @@
+// note use of preInit & postInit will run for EVERY mission, use sparingly or with non a3a mission aborts in place, example check if the class (missionConfigFile >> "A3A") exists
 class CfgFunctions
 {
     class A3A
--- A3A/addons/maps/MissionDescription/master.hpp
+++ A3A/addons/maps/MissionDescription/master.hpp
@@ -6,6 +6,8 @@ author = $STR_antistasi_credits_generic_author_text;
 Keys[] = {"A3-Antistasi-is-not-available-in-single-player"};
 KeysLimit = 2;  // Even if player tampers with his unlocked keys, this will never become true.
 
+class A3A {}; //used mainly to detect that the mission is a A3A mission, can also contain other data in the future
+
 #include "debug.hpp"
 #include "gameSettings.hpp"
 #include "params.hpp"
